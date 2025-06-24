2ffd9b85371fd6faf12828dae61f800c9088127e
“[Marlon <marlondossantosklain@gmail.com>
2025-06-23 23:23:38 -0300

    Create the root structure for the project

---
+++ src/main/java/com/moneymaster/moneymaster/model/entity/Budget.java
@@ -0,0 +1,9 @@
+package com.moneymaster.moneymaster.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Budgets")
+public class Budget {
+}
---
+++ src/main/java/com/moneymaster/moneymaster/model/entity/User.java
@@ -0,0 +1,32 @@
+package com.moneymaster.moneymaster.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private UUID userId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "username")
+    private String username;
+
+
+}
