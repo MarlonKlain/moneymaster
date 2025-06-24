f567564963a6393075adbb0f5c6da476c9a82c0e
“[Marlon <marlondossantosklain@gmail.com>
2025-06-23 22:41:18 -0300

    Initial commit

---
+++ src/test/java/com/moneymaster/moneymaster/MoneymasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneymaster.moneymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
