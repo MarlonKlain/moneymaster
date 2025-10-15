c3dde0da9ea5e5657256c3ea09e0e74855ea306d
“[Marlon <marlondossantosklain@gmail.com>
2025-10-14 22:50:14 -0300

    debugging

--- src/main/java/com/moneymaster/moneymaster/MoneymasterApplication.java
+++ src/main/java/com/moneymaster/moneymaster/MoneymasterApplication.java
@@ -1,7 +1,10 @@
 package com.moneymaster.moneymaster;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MoneymasterApplication {
@@ -10,4 +13,13 @@ public class MoneymasterApplication {
 		SpringApplication.run(MoneymasterApplication.class, args);
 	}
 
+	// --- ADD THIS BEAN TO DEBUG ---
+	@Bean
+	CommandLineRunner commandLineRunner(Environment env) {
+		return args -> {
+			System.out.println("=================================================");
+			System.out.println(">>>>>> [DEBUG] SPRING_DATASOURCE_URL from Environment: " + env.getProperty("SPRING_DATASOURCE_URL"));
+			System.out.println("=================================================");
+		};
+	}
 }
