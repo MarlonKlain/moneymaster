825435720533c511b2d2ac29ee32f882fdddced7
MarlonKlain <marlondossantosklain@gmail.com>
2025-10-15 21:37:01 -0300

    Update V1__create_tables.sql

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -4,5 +4,5 @@ CREATE TABLE users (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE
 );
